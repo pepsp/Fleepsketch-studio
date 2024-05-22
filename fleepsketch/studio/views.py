a4a60a835f0c88078b8e7f5d18bd2b0dc8bb904c
pepsp <josec1000_0@hotmail.com>
2024-05-22 15:39:45 -0700

    first commit

---
+++ fleepsketch/studio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'studio/layout.html')
